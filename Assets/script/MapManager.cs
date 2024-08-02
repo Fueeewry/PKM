20e1771bbfcb531fa3a6c589b8a698df57083216
Kevinjomin <122532032+Kevinjomin@users.noreply.github.com>
2024-08-02 21:32:07 +0700

    ehe

--- Assets/script/MapManager.cs
+++ Assets/script/MapManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -16,6 +17,10 @@ public class MapManager : MonoBehaviour
     [SerializeField] private Sprite mapButtonEnabledImg;
     [SerializeField] private Sprite mapButtonDisabledImg;
 
+    public Material lineMaterial;
+
+    [ColorUsage(false, true)] public List<Color> lineColors = new List<Color>();
+
     private void Awake()
     {
         if (Instance == null) Instance = this;
@@ -26,11 +31,20 @@ public class MapManager : MonoBehaviour
         mapButton.onClick.AddListener(()=> MapButton());
     }
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse1) && MapCanvas.activeSelf == true) MaterialColorRandomizer();
+    }
+
     public void SetMapDisplay(bool boolean)
     {
         if(MapCanvas != null)
         {
             MapCanvas.SetActive(boolean);
+        }
+        if(boolean == true)
+        {
+            MaterialColorRandomizer();
         }
     }
 
@@ -58,5 +72,31 @@ public class MapManager : MonoBehaviour
         {
             SetMapDisplay(true);
         }
+    }
+
+    public void MaterialColorRandomizer()
+    {
+        int random = 0;
+        do
+        {
+            random = UnityEngine.Random.Range(0, lineColors.Count);
+        } 
+        while (lineMaterial.color == lineColors[random]);
+
+
+        StartCoroutine(ColorLerp(random));
+
+        
+        //lineMaterial.color = lineColors[random];
+    }
+
+    public IEnumerator ColorLerp(int random)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            lineMaterial.color = Color.Lerp(lineMaterial.color, lineColors[random], 0.2f);
+            yield return new WaitForSeconds(0.02f);
+        }
+        
     }
 }
